60b639194364a9cc025dc140ad7b0642de8c4f2e
Eric2422 <ericbkzhang@gmail.com>
2023-11-23 23:57:27 -0500

    Wrote some simple code to test autonomous

--- autonomous.java
+++ autonomous.java
@@ -1,15 +1,81 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.*;
 import com.qualcomm.robotcore.hardware.*;
 
 @Autonomous(name = "Autonomous")
-@Disabled
 public class autonomous extends LinearOpMode {
+    // constants for how far the arm can extend and retract
+    private final int ARM_EXTEND_LIMIT = 0;
+    private final int ARM_RETRACT_LIMIT = 0;
+
+    // constants for how far the arm can rotate up and down
+    private final int ARM_ROTATE_MAX = 2000;
+    private final int ARM_ROTATE_MIN = 0;
+    private final int ARM_ROTATE_MID = (this.ARM_ROTATE_MAX + this.ARM_ROTATE_MIN) / 2;
+
+    // constant for the speed that the arm rotates with
+    private final double ARM_ROTATIONAL_VELOCITY = 100;
+
+    // constant for the speed that the arm extends and retracts with
+    private final double ARM_EXTEND_SPEED = 0.5;
+
+    // constants for the open and closed positions of the claw
+    private final double CLAW_OPEN_POSITION = 0.2;
+    private final double CLAW_CLOSE_POSITION = 0.1;
+
+    // the DC motors for the wheels
+    private DcMotor leftWheelMotor, rightWheelMotor;
+
+    // the DC motors for the arm
+    private DcMotor armRotationMotor, armExtensionMotor;
+
+    // the servo motors for the pincers of the claw
+    private Servo pincerServo;
+
+    // the servo that rotates the claw back and forth
+    private Servo clawRotationServo;
 
     @Override
     public void runOpMode() {
-        
+        // assigning the motors variables to the configured names on the driver hub
+        this.leftWheelMotor = hardwareMap.get(DcMotor.class, "left_motor");
+        this.rightWheelMotor = hardwareMap.get(DcMotor.class, "right_motor");
+
+        this.armRotationMotor = hardwareMap.get(DcMotor.class, "arm_rotator");
+        this.armExtensionMotor = hardwareMap.get(DcMotor.class, "arm_extender");
+
+        this.pincerServo = hardwareMap.get(Servo.class, "pincer_servo");
+
+        // setting the direction of the motors
+        // rightWheelMotor and armRotationMotor are forward by default
+        this.leftWheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // setting the two pincer servo positions to 1, which is upright, aka not
+        // pressing the claw
+        this.pincerServo.setPosition(this.CLAW_OPEN_POSITION);
+
+        // set the servo position of the grabber rotator to 1.0
+        this.clawRotationServo.setPosition(1.0);
+
+        // wait for the player to press start
+        waitForStart();
+
+        // drive forward and backwards to test
+        setWheelPower(1.0);
+
+        sleep(1000);
+
+        setWheelPower(0.0);
+
+        telemetry.addData("", "Done running!");
+    }
+
+    /*
+     * Set the power of the wheels to the same value
+     */
+    public void setWheelPower(double wheelPower) {
+        this.leftWheelMotor.setPower(wheelPower);
+        this.rightWheelMotor.setPower(wheelPower);
     }
 }
