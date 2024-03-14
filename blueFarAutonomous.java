5802002fdd5bc908c621bfd263e4fef0024f183c
unknown <you@example.com>
2024-03-14 16:20:01 -0400

    Add a test for autoSleep to blueFarAutonomous
    If it works, the arm will raise then lower.
    If not, it will do nothing.

--- blueFarAutonomous.java
+++ blueFarAutonomous.java
@@ -1,4 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
-public class blueFarAutonomous {
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous(name = "Blue Far Autonomous")
+public class blueFarAutonomous extends LinearOpMode {
+    private Hardware hardware = new Hardware(this);
+
+    /**
+     * Automatically runs after pressing init()
+     */
+    @Override
+    public void runOpMode() {
+        hardware.init();
+
+        hardware.rotateArm(90);
+        hardware.rotateArm(0);
+    }
+
+    /**
+     * Inititialize the variables for Tfod
+     */
+    public void initTfod() {
+        // create a TensorFlow processor
+        // robot.tfod = new TfodProcessor.Builder().build();
+
+        // create a VisionPortal
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+    }
+
+    /**
+     * Push the pixel onto the tape
+     */
+    public void pushPixel() {
+    }
+
+    /**
+     * Drive to the scoring board
+     */
+    public void driveToBoard() {
+    }
+
+    /**
+     * Rotates the claw backward and drops the pixel
+     */
+    public void dropPixel() {
+    }
+
+    /**
+     * Moves the arm back to initial position
+     */
+    public void returnArmToInitialPosition() {
+    }
 }
